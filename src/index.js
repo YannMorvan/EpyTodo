9426ecc5ceb852eff5f673fdf2881bd4bc5a2c1c
Adrien Philibert <adrien.philibert@epitech.eu>
2023-05-03 02:14:56 +0200

    UPDATE: index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
-const express = require ("express") ;
-const env = require ("../.env") ;
-const app = express () ;
-const port = process.env.PORT | 3000;
+const express = require ("express");
+const dotenv = require ('dotenv');
+const app = express ();
+const port = 3000;
+dotenv.config()
 
-app.get("/", (req, res) => {
-    res.send("Hello World !\n");
-});
-
-app.get("/name/:name", (req, res) => {
-    res.send(`Hello ${req.params.name} !\n`);
-});
-
-app.get("/date", (req, res) => {
-    let date = new Date();
-    res.send(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}\n`);
-});
+require('./routes/auth/auth');
+require('./routes/todos/todos');
+require('./routes/user/user');
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
