13fd8fc24c1a77aa807aef8c3cf70f5e0df86f0c
Yann Morvan <yann.morvan@epitech.eu>
2023-05-10 16:12:59 +0100

    Add Not found UserId and TodoId

---
+++ src/middleware/notFound.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.get('/todos/:id', (req, res) => {
+    const todoId = req.params.id;
+    const todo = findTodoById(todoId);
+
+    if (!todo)
+        res.status(404).json({msg: 'Not found'});
+});
+
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id;
+    const user = findUserById(userId);
+
+    if (!user)
+        res.status(404).json({msg: 'Not found'});
+});
+
+module.exports = router;
