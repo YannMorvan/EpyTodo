6df3b7e0e7bd038ad2590e746393ef4fade30425
Adrien Philibert <adrien.philibert@epitech.eu>
2023-05-21 19:34:28 +0200

    FEAT: user && UPDATE: setHeader

--- src/index.js
+++ src/index.js
@@ -5,14 +5,13 @@ const app = express();
 dotenv.config();
 
 const auth = require('./middleware/auth');
-const login = require('./middleware/auth');
-const authRoutes = require('./routes/auth/auth');
-const userRouter = require('./routes/user/user');
+const { userRouter, usersRouter } = require('./routes/user/user');
 const todosRoutes = require('./routes/todos/todos');
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     next();
 });
 
@@ -21,16 +20,16 @@ app.use(express.json({
         try {
             JSON.parse(buf);
         } catch (error) {
-            res.status(400).json({ msg: "Bad parameter" });
+            res.status(400).json({ msg: 'Bad parameter' });
         }
     }
 }));
 
 app.use('/', require('./routes/auth/auth.js'));
-// app.use('/user', auth, userRouter);
+app.use('/user', auth, userRouter);
 // app.use('/todos', auth, todosRoutes);
 
-app.use("*", (req, res) => res.status(404).json({ msg: "Not Found" }));
+app.use("*", (req, res) => res.status(404).json({ msg: 'Not Found' }));
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
