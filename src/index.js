36682ca3b6a1d7891bb3a71060cea972412d9c37
Adrien Philibert <adrien.philibert@epitech.eu>
2023-05-21 17:17:30 +0200

    FIX: error message

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = express();
 dotenv.config();
 
 const auth = require('./middleware/auth');
+const login = require('./middleware/auth');
 const authRoutes = require('./routes/auth/auth');
 const userRouter = require('./routes/user/user');
 const todosRoutes = require('./routes/todos/todos');
@@ -20,16 +21,16 @@ app.use(express.json({
         try {
             JSON.parse(buf);
         } catch (error) {
-            res.status(400).json({ msg: 'Bad parameter' });
+            res.status(400).json({ msg: "Bad parameter" });
         }
     }
 }));
 
 app.use('/', require('./routes/auth/auth.js'));
-//app.use('/user', auth, userRouter);
-//app.use('/todos', auth, todosRoutes);
+// app.use('/user', auth, userRouter);
+// app.use('/todos', auth, todosRoutes);
 
-app.use("*", (req, res) => res.status(404).json({ msg: 'Not Found' }));
+app.use("*", (req, res) => res.status(404).json({ msg: "Not Found" }));
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
