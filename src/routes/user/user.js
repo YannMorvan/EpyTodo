1cd8e84155b2d1487c0df207e3878074a1ea8969
Adrien Philibert <adrien.philibert@epitech.eu>
2023-05-21 19:32:39 +0200

    FEAT: all users features

---
+++ src/routes/user/user.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const { check_user_id } = require('../../middleware/notFound');
+const regex = require('../../config/regex');
+const { getAllTodoByUser, getUserById, getUserByEmail, modifyUserById, deleteUserById } = require('./user.query');
+const userRouter = express.Router();
+const usersRouter = express.Router();
+
+userRouter.get('/', (req, res) => {
+    getUserById(res, req.userID.id);
+});
+
+userRouter.get('/todos', (req, res) => {
+    getAllTodoByUser(res, req.userID.id);
+});
+
+usersRouter.param('id', (req, res, next, id) => {
+    if (isNaN(id))
+        next('route');
+    else
+        next();
+});
+
+usersRouter.param('email', (req, res, next, id) => {
+    if (id.match(regex))
+        next();
+    else
+        next('route');
+});
+
+usersRouter.get('/email/:email', (req, res) => {
+    var email = req.params.email;
+
+    if (!email)
+        return res.status(400).json({ msg: 'Invalid Credentials' });
+    getUserByEmail(res, email);
+});
+
+usersRouter.get('/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!id)
+        return res.status(400).json({ msg: 'Invalid Credentials' });
+    getUserById(res, id);
+});
+
+usersRouter.put('/:id', check_user_id, (req, res) => {
+    var email = req.body.email;
+    var password = req.body.password;
+    var firstname = req.body.firstname;
+    var name = req.body.name;
+    var id = req.params.id;
+
+    if (!id || !email || !password || !firstname || !name || !email.match(regex))
+        return res.status(401).json({ msg: 'Invalid Credentials' });
+    modifyUserById(res, id, email, password, firstname, name);
+});
+
+usersRouter.delete('/:id', check_user_id, (req, res) => {
+    var id = req.params.id;
+
+    if (!id)
+        return res.status(400).json({ msg: 'Invalid Credentials' });
+    deleteUserById(res, req.params.id);
+});
+
+module.exports = { userRouter, usersRouter };
---
+++ src/routes/user/user.query.js
@@ -0,0 +1,96 @@
+const db = require('../../config/db');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+exports.register = (res, email, name, firstname, password) => {
+    bcrypt.hash(password, 10)
+    .then(hash => {
+        db.execute('INSERT INTO `user` (email, password, name, firstname) VALUES (?, ?, ?, ?)', [email, hash, name, firstname], (error, results, fields) => {
+            if (error) {
+                if (error.code == 'ER_DUP_ENTRY')
+                    res.status(400).json({ msg: 'Account already exists' });
+                else
+                    res.status(500).json({ msg: 'Internal server error' });
+                return;
+            }
+            const token = jwt.sign(
+                { id: results.insertId },
+                process.env.EPYTODO_SECRET,
+                { expiresIn: '24h' }
+            );
+            res.status(201).json({ token: token });
+        });
+    })
+    .catch(() => res.status(500).json({ msg: 'Internal server error' }));
+};
+
+exports.login = (res, email, password) => {
+    db.execute('SELECT * FROM `user` WHERE `email` = ?', [email], (error, results, fields) => {
+        if (results.length != 1 || error)
+            return res.status(401).json({ msg: 'Invalid Crendentials' });
+        bcrypt.compare(password, results[0].password)
+            .then(valid => {
+                if (!valid)
+                    return res.status(401).json({ msg: 'Invalid Credentials' });
+                const token = jwt.sign(
+                    { id: results[0].id },
+                    process.env.EPYTODO_SECRET,
+                    { expiresIn: '24h' }
+                );
+                res.status(200).json({ token: token });
+            })
+            .catch(() => res.status(500).json({ msg: 'Internal server error' }));
+    });
+};
+
+exports.getAllUsers = (res) => {
+    db.execute('SELECT CAST(`id` AS VARCHAR(10)) AS `id`, `email`, `password`, DATE_FORMAT(`created_at`, "%Y-%m-%d %H:%i:%S") AS `created_at`, `firstname`, `name` FROM `user`', (error, results, fields) => {
+        if (error)
+            return res.status(500).json({ msg: 'Internal server error' });
+        res.status(200).json(results);
+    });
+};
+
+exports.getAllTodoByUser = (res, user_id) => {
+    db.execute('SELECT CAST(`id` AS VARCHAR(10)) AS `id`, `title`, `description`, DATE_FORMAT(`created_at`, "%Y-%m-%d %H:%i:%S") AS `created_at`, DATE_FORMAT(`due_time`, "%Y-%m-%d %H:%i:%S") AS `due_time`, CAST(`user_id` AS VARCHAR(10)) AS `user_id`, `status` FROM `todo` WHERE `user_id` = ?', [user_id], (error, results, fields) => {
+        if (error)
+            return res.status(500).json({ msg: 'Internal server error' });
+        res.status(200).json( results );
+    });
+};
+
+exports.getUserById = (res, user_id) => {
+    db.execute('SELECT CAST(`id` AS VARCHAR(10)) AS `id`, `email`, `password`, DATE_FORMAT(`created_at`, "%Y-%m-%d %H:%i:%S") AS `created_at`, `firstname`, `name` FROM `user` WHERE `id` = ?', [user_id], (error, results, fields) => {
+        if (error)
+            return res.status(500).json({ msg: 'Internal server error' });
+        if (results.length < 1)
+            return res.status(404).json({ msg: 'Not found' });
+        res.status(200).json( results[0] );
+    });
+};
+
+exports.getUserByEmail = (res, email) => {
+    db.execute('SELECT CAST(`id` AS VARCHAR(10)) AS `id`, `email`, `password`, DATE_FORMAT(`created_at`, "%Y-%m-%d %H:%i:%S") AS `created_at`, `firstname`, `name` FROM `user` WHERE `email` = ?', [email], (error, results, fields) => {
+        if (error)
+            return res.status(500).json({ msg: 'Internal server error' });
+        if (results.length < 1)
+            return res.status(404).json({ msg: 'Not found' });
+        res.status(200).json( results[0] );
+    });
+};
+
+exports.modifyUserById = (res, user_id, email, password, firstname, name) => {
+    db.execute('UPDATE `user` SET email = ?, password = ?, firstname = ?, name = ? WHERE id = ?', [email, password, firstname, name, user_id], (error, results, fields) => {
+        if (error)
+            return res.status(500).json({ msg: 'Internal server error' });
+        this.getUserById(res, user_id);
+    });
+};
+
+exports.deleteUserById = (res, user_id) => {
+    db.execute('DELETE FROM `user` WHERE `id` = ?', [user_id], (error, results, fields) => {
+        if (error)
+            return res.status(500).json({ msg: 'Internal server error' });
+        res.status(200).json({ msg: `Successfully deleted record number: ${user_id}` });
+    });
+};
